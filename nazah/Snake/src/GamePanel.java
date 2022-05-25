2669b92aeac58310212e3b2b0944e3c7d36a9767
Ishrar2019331104 <69473062+Ishrar2019331104@users.noreply.github.com>
2022-05-26 01:09:07 +0600

    Update GamePanel.java

--- nazah/Snake/src/GamePanel.java
+++ nazah/Snake/src/GamePanel.java
@@ -10,12 +10,12 @@ import java.util.Random;
 
 public class GamePanel extends JPanel implements ActionListener{
 
-    static final int SCREEN_WIDTH = 600;
+    static final int SCREEN_WIDTH = 900;
     static final int SCREEN_HEIGHT = 600;
     static final int UNIT_SIZE = 25; /// how big we want the objects in our game. basically 25 pixels in this screen
     static final int GAME_UNITS = (SCREEN_WIDTH*SCREEN_HEIGHT)/UNIT_SIZE; /// number of objects in the screen
 
-    static final int DELAY = 200; /// delay for our timer. this timer will call the action listener after a delay
+    static final int DELAY = 150; /// delay for our timer. this timer will call the action listener after a delay
     final int x[] = new int[GAME_UNITS]; // body part of the snake, the snake cannot be bigger than the game itself
     final int y[] = new int[GAME_UNITS];
 
@@ -68,6 +68,7 @@ public class GamePanel extends JPanel implements ActionListener{
         paused = false;
         newApple();
         running = true; // game is running now
+        generateMaze();
         timer = new Timer(DELAY,this); /// action listener interfsce
 
 //        for (int i = 0; i < 10; i++) {
@@ -75,7 +76,10 @@ public class GamePanel extends JPanel implements ActionListener{
 //            mazeParts.add(new Point(mazeCoord[0], mazeCoord[1]));        }
         timer.start();
     }
+    public void generateMaze()
+    {
 
+    }
     public void paintComponent(Graphics g)
     {
         super.paintComponent(g); /// eitay ki korbe bujhinai :v
@@ -133,24 +137,50 @@ public class GamePanel extends JPanel implements ActionListener{
             // draw rect
 
 
-            walls.add(new Maze(50,25,75,25)); ///a
+            walls.add(new Maze(50,25,100,25)); ///a
             walls.add(new Maze(50,50,25,175)); // b
             walls.add(new Maze(25,100,25,25)); /// c
             walls.add(new Maze(75,200,150,25)); /// d
             walls.add(new Maze(200,175,25,25)); /// e
-            walls.add(new Maze(125,125,25,50)); /// f
-            walls.add(new Maze(150,125,100,25)); /// g
-            walls.add(new Maze(225,25,25,100)); /// h
-            walls.add(new Maze(250,25,75,25)); /// i
-            walls.add(new Maze(325,25,25,275)); //j
-            walls.add(new Maze(350,275,50,25)); // k
-            walls.add(new Maze(400,175,25,125)); // l
-            walls.add(new Maze(425,175,50,25)); // m
-            walls.add(new Maze(475,175,25,50)); // n
-            walls.add(new Maze(475,225,75,25)); /// o
-            walls.add(new Maze(525,250,25,200)); // p
-            walls.add(new Maze(550,325,25,25)); // q
-            walls.add(new Maze(425,225,50,25));
+            walls.add(new Maze(100+25*10,125,25,75)); /// f
+            walls.add(new Maze(125+25*10,125,125,25)); /// g
+            walls.add(new Maze(225+25*10,25,25,100)); /// h
+            walls.add(new Maze(250+25*10,375,25,200));
+            walls.add(new Maze(250+25*10,25,75,25)); /// i
+            walls.add(new Maze(325+25*10,25,25,275)); //j
+            walls.add(new Maze(350+25*10,275,75,25)); // k
+            walls.add(new Maze(425+25*10,175,25,125)); // l
+            walls.add(new Maze(425+25*10,175,100,25)); // m
+            walls.add(new Maze(525+25*10,175,25,50)); // n
+            walls.add(new Maze(525+25*10,225,75,25)); /// o
+            walls.add(new Maze(575+25*10,250,25,200)); // p
+            walls.add(new Maze(600+25*10,325,25,25)); // q
+            walls.add(new Maze(425+25*10,425,100,25)); // r
+            walls.add(new Maze(425+25*10,450,25,100)); // s
+            walls.add(new Maze(375+25*10,550,75,25)); // t
+            walls.add(new Maze(200,225,25,125)); // u
+            walls.add(new Maze(225,325,200,25)); //v
+            walls.add(new Maze(300,350,25,175)); ///w
+            walls.add(new Maze(250,500,50,25)); // x
+            walls.add(new Maze(25,275,75,25)); // y
+            walls.add(new Maze(25,400,200,25)); // z
+            walls.add(new Maze(75,350,25,50)); // AA
+            walls.add(new Maze(25,425,25,75)); // BB
+            walls.add(new Maze(100,500,100,25)); // CC
+            walls.add(new Maze(175,525,25,50)); /// DD
+            walls.add(new Maze(500+25*10,25,100,25)); /// EE
+            walls.add(new Maze(575+25*10,50,25,50)); // FF
+            walls.add(new Maze(475+25*10,100,125,25)); // GG
+
+
+
+
+
+
+
+
+
+
 
 
             for(Maze wall: walls)
@@ -179,12 +209,12 @@ public class GamePanel extends JPanel implements ActionListener{
                     g.fillOval(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
                 }
             }
-            g.setColor(new Color(0xC51D2A));
-            g.setFont( new Font("Courier New",Font.BOLD, 30));
-            FontMetrics metrics = getFontMetrics(g.getFont());
-
-
-            g.drawString("Score: "+applesEaten, (SCREEN_WIDTH - metrics.stringWidth("Score: "+applesEaten))/2, g.getFont().getSize());
+//            g.setColor(new Color(0xC51D2A));
+//            g.setFont( new Font("Courier New",Font.BOLD, 30));
+//            FontMetrics metrics = getFontMetrics(g.getFont());
+//
+//
+//            g.drawString("Score: "+applesEaten, (SCREEN_WIDTH - metrics.stringWidth("Score: "+applesEaten))/2, g.getFont().getSize());
         }
         else
         {
@@ -194,9 +224,28 @@ public class GamePanel extends JPanel implements ActionListener{
     }
     public void newApple() /// everytime apple khabe notun location generate korbe
     {
+//        appleX = 300; /// placed evenly within the boxes
+//        appleY = 300;
+        boolean conflict;
+        do {
+            conflict = false;
+            appleX = random.nextInt((int) SCREEN_WIDTH / UNIT_SIZE) * UNIT_SIZE; /// placed evenly within the boxes
+            appleY = random.nextInt((int) SCREEN_HEIGHT / UNIT_SIZE) * UNIT_SIZE;
+            for (Maze wall : walls) {
+                if (wall.hitBox.x == appleX && wall.hitBox.y == appleY) {
+                    conflict = true;
+                }
+            }
+        } while (conflict);
 
-        appleX = random.nextInt((int)SCREEN_WIDTH/UNIT_SIZE)*UNIT_SIZE; /// placed evenly within the boxes
-        appleY = random.nextInt((int)SCREEN_HEIGHT/UNIT_SIZE)*UNIT_SIZE;
+//        for(Maze wall: walls) {
+//            while (wall.hitBox.x != appleX && wall.hitBox.y != appleY) {
+//                appleX = random.nextInt((int) SCREEN_WIDTH / UNIT_SIZE) * UNIT_SIZE; /// placed evenly within the boxes
+//                appleY = random.nextInt((int) SCREEN_HEIGHT / UNIT_SIZE) * UNIT_SIZE;
+//            }
+
+//    }
+
     }
     public void move()
     {
@@ -220,6 +269,7 @@ public class GamePanel extends JPanel implements ActionListener{
     }
     public void checkApple() // if apple khaise
     {
+
         if(x[0]==appleX && y[0] == appleY)
         {
             bodyParts++;
@@ -282,10 +332,10 @@ public class GamePanel extends JPanel implements ActionListener{
 
     public void gameOver(Graphics g) /// new popup
     {
-        g.setColor(new Color(0xC51D2A));
-        g.setFont( new Font("Courier New",Font.BOLD, 30));
-        FontMetrics metrics1 = getFontMetrics(g.getFont());
-        g.drawString("Score: "+applesEaten, (SCREEN_WIDTH - metrics1.stringWidth("Score: "+applesEaten))/2, g.getFont().getSize());
+//        g.setColor(new Color(0xC51D2A));
+//        g.setFont( new Font("Courier New",Font.BOLD, 30));
+//        FontMetrics metrics1 = getFontMetrics(g.getFont());
+//        g.drawString("Score: "+applesEaten, (SCREEN_WIDTH - metrics1.stringWidth("Score: "+applesEaten))/2, g.getFont().getSize());
         /// game over - text
         g.setColor(new Color(0x1d1817));
         g.setFont(new Font("Courier New",Font.ITALIC,75));
